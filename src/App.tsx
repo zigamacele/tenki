e14994e2d74b3eb215414bf36d1413bfb2510317
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-08-17 18:36:28 +0200

    feat: added Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import Input from './components/Input'
 import WeatherDisplay from './components/WeatherDisplay'
 import SearchHistory from './components/SearchHistory'
+import Header from './components/Header'
 const App: React.FC = () => {
   const [search, setSearch] = useState('Seoul')
 
   return (
-    <main className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-      <SearchHistory search={search} />
+    <main className='absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2  flex-col gap-2'>
       <Input setSearch={setSearch} />
-      <WeatherDisplay search={search} />
+      <section className='border-rounded w-full rounded-lg border border-neutral-200 bg-white fade-in sm:h-[40em] sm:w-[30em]'>
+        <Header search={search} />
+        <SearchHistory search={search} />
+        <WeatherDisplay search={search} />
+      </section>
     </main>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import wave from '@/assets/wave.svg'
+
+interface HeaderProps {
+  search: string
+}
+
+const Header: React.FC<HeaderProps> = ({ search }) => {
+  return (
+    <section className='relative m-2 overflow-hidden'>
+      <div className='absolute bottom-0 z-10 h-full w-full bg-gradient-to-t from-neutral-50 to-transparent opacity-30'></div>
+      <img
+        src={`https://source.unsplash.com/random/?${search}`}
+        key={search}
+        className='slide-from-bottom z-20 h-[25em] w-full rounded-t-md object-cover'
+      />
+      <img src={wave} className='absolute bottom-0 w-full' />
+    </section>
+  )
+}
+
+export default Header
