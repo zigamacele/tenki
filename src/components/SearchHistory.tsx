0de4ddb3c8386bb8c4b7f8f7fee5bf888a451ecd
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-08-18 18:51:08 +0200

    fix: search history going out of bounds

--- src/components/SearchHistory.tsx
+++ src/components/SearchHistory.tsx
@@ -15,12 +15,12 @@ const SearchHistory: React.FC<SearchHistoryProps> = ({ search, setSearch }) => {
   }, [search])
 
   return (
-    <section className='flex items-center gap-1.5 fade-in'>
-      <History className='h-5 w-5' />
+    <section className='flex w-full items-center gap-1.5 truncate fade-in'>
+      <History className='h-5 w-5 shrink-0' />
       {history.slice(-5).map((item, index) => (
         <div
           key={index}
-          className='slide-from-left cursor-pointer rounded-lg border border-neutral-300 bg-neutral-200 px-2 py-0.5 text-xs capitalize text-neutral-600 hover:border-neutral-500 hover:bg-neutral-300'
+          className='slide-from-left max-w-[4em] cursor-pointer truncate text-ellipsis rounded-lg border border-neutral-300 bg-neutral-200 px-2 py-0.5 text-xs capitalize text-neutral-600 hover:border-neutral-500 hover:bg-neutral-300 sm:max-w-[7em]'
           onClick={() => setSearch(item)}
         >
           {item}
